94463f41b378c4bd6aa7ba88df974ccea7505047
Arshad Ali <arshadali99936@gmail.com>
2025-07-17 13:48:15 +0530

    Update 2.Inserting data in table.sql
    fixed lines

--- 2.Inserting data in table.sql
+++ 2.Inserting data in table.sql
@@ -5,12 +5,4 @@ INSERT INTO employees(name, position, department, hire_date, salary)
 		('Shameem Banu', 'Data Analyst', 'Data Science', '2023-05-15', 75000.00),
 		('Nishat Anjum', 'DevOps Engineer', 'CSE', '2023-07-10', 85000.00);
 
-SELECT COUNT(*) FROM employees;
-
-DELETE FROM employees
-WHERE name IN ('Arshad', 'Shameem Banu', 'Nishat Anjum');
-
-TRUNCATE TABLE employees;
-
-TRUNCATE TABLE employees RESTART IDENTITY;
 
