b3b1568396a097b764a59f43058e9ae1a945da59
Arshh26 <arshadali99936@gmail.com>
2025-07-18 20:44:09 +0530

    Added lesson

---
+++ 3. Deleting rows, and columns from Table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE USERS (
+		Users_id INT PRIMARY KEY,
+		Name Varchar(100) NOT NULL,
+		Qualification varchar(100),
+		Age INT,
+		Income NUMERIC(10, 2)
+);
+
+SELECT * FROM Users;
+
+INSERT INTO Users(Users_id, Name, Qualification, Age, Income)
+		VALUES('5662', 'Sathish', 'Sub-Inspector', '30', 78000.00),
+		('5663', 'Dhawale', 'Professor', '28', 40000.00),
+		('5664', 'Gupta', 'Lecturer', '30', 38000.00),
+		('5665', 'Ganesh', 'Peon', '32', 25000.00);
+
+DELETE FROM Users
+WHERE Users_id= 5664;
+
+ALTER TABLE Users
+DROP COLUMN Age;
+
+
