101a8a41b8fe0d5b32a440bfcc7d5d87b61418e9
Arshh26 <arshadali99936@gmail.com>
2025-07-28 00:36:45 +0530

    Initial Update

--- 5 Update Data in SQL.sql
+++ 5 Update Data in SQL.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS User1;
-
-CREATE TABLE IF NOT EXISTS User1 (
+CREATE TABLE IF NOT EXISTS User3 (
 		employee_id INT PRIMARY KEY,
 		Name VARCHAR(100) NOT NULL,
 		Age INTEGER CHECK (age>=18),
@@ -11,27 +9,51 @@ CREATE TABLE IF NOT EXISTS User1 (
 		Salary NUMERIC(10,2)
 );
 
-INSERT INTO User1(employee_id, Name, Age, Email, Position, Department, Hire_Date, Salary)
-		VALUES('1001', 'Arshad','21','arshadali@gmail.com', 'Software Engineer', 'CSE','2023-08-08', 65000.00),
-		('1002', 'Shameem Banu', '24','nishatanjum1@Yahoo.com', 'Data Analyst', 'Data Science', '2023-05-15', 75000.00),
-		('1004', 'Nishat Anjum','28','shameembanu2@gmsil.com', 'DevOps Engineer', 'CSE', '2023-07-10', 85000.00);
+INSERT INTO User3(employee_id, Name, Age, Email, Position, Department, Hire_Date, Salary)
+		VALUES('1005', 'Arshad','21','arshadali1@gmail.com', 'Software Engineer', 'CSE','2023-08-08', 65000.00),
+		('1006', 'Shameem Banu', '24','nishatanjum2@Yahoo.com', 'Data Analyst', 'Data Science', '2023-05-15', 75000.00),
+		('1007', 'Nishat Anjum','28','shameembanu3@gmsil.com', 'DevOps Engineer', 'CSE', '2023-07-10', 85000.00);
 
-SELECT * FROM User1;
+SELECT * FROM User3;
 
-Update User1
+Update User3
 SET age = 21
 WHERE Name = 'Shameem Banu';
 
-SELECT * FROM User1 order by employee_id ASC;
+SELECT * FROM User3 order by employee_id ASC;
 
-UPDATE User1
+UPDATE User3
 SET position='Software Engineer'
 WHERE salary=85000;
 
-UPDATE User1
+UPDATE User3
 SET age = 22, department = 'Data Analyst'
-WHERE name = 'Shameem Banu'
+WHERE name = 'Shameem Banu';
 
-UPDATE User1
+UPDATE User3
 SET salary=salary+10000
 WHERE email LIKE '%@gmail.com';
+
+-- To Rename the name column to full_name
+
+ALTER TABLE User3
+RENAME COLUMN name to full_name;
+
+SELECT * FROM Customer1;
+
+-- To Rename the age DataType integer to smallint
+
+ALTER TABLE User3
+ALTER COLUMN age TYPE smallint;
+
+-- To set the constraints
+
+ALTER TABLE User3
+ALTER COLUMN department SET NOT NULL;
+
+ALTER TABLE User3
+ADD CONSTRAINT age CHECK (age >= 18);
+
+-- To Rename the Table Name 
+ALTER TABLE User2
+RENAME TO Customer1;
