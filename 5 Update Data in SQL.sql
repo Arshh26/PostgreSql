a3f705b7454784c0b3c34e66e07a97a75fdc4973
Arshh26 <arshadali99936@gmail.com>
2025-07-28 00:41:44 +0530

    Updated

--- 5 Update Data in SQL.sql
+++ 5 Update Data in SQL.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS User3 (
+CREATE TABLE IF NOT EXISTS User2 (
 		employee_id INT PRIMARY KEY,
 		Name VARCHAR(100) NOT NULL,
 		Age INTEGER CHECK (age>=18),
@@ -9,51 +9,27 @@ CREATE TABLE IF NOT EXISTS User3 (
 		Salary NUMERIC(10,2)
 );
 
-INSERT INTO User3(employee_id, Name, Age, Email, Position, Department, Hire_Date, Salary)
+INSERT INTO User2(employee_id, Name, Age, Email, Position, Department, Hire_Date, Salary)
 		VALUES('1005', 'Arshad','21','arshadali1@gmail.com', 'Software Engineer', 'CSE','2023-08-08', 65000.00),
 		('1006', 'Shameem Banu', '24','nishatanjum2@Yahoo.com', 'Data Analyst', 'Data Science', '2023-05-15', 75000.00),
 		('1007', 'Nishat Anjum','28','shameembanu3@gmsil.com', 'DevOps Engineer', 'CSE', '2023-07-10', 85000.00);
 
-SELECT * FROM User3;
+SELECT * FROM User2;
 
-Update User3
+Update User2
 SET age = 21
 WHERE Name = 'Shameem Banu';
 
-SELECT * FROM User3 order by employee_id ASC;
+SELECT * FROM User2 order by employee_id ASC;
 
-UPDATE User3
+UPDATE User2
 SET position='Software Engineer'
 WHERE salary=85000;
 
-UPDATE User3
+UPDATE User2
 SET age = 22, department = 'Data Analyst'
 WHERE name = 'Shameem Banu';
 
-UPDATE User3
+UPDATE User2
 SET salary=salary+10000
 WHERE email LIKE '%@gmail.com';
-
--- To Rename the name column to full_name
-
-ALTER TABLE User3
-RENAME COLUMN name to full_name;
-
-SELECT * FROM Customer1;
-
--- To Rename the age DataType integer to smallint
-
-ALTER TABLE User3
-ALTER COLUMN age TYPE smallint;
-
--- To set the constraints
-
-ALTER TABLE User3
-ALTER COLUMN department SET NOT NULL;
-
-ALTER TABLE User3
-ADD CONSTRAINT age CHECK (age >= 18);
-
--- To Rename the Table Name 
-ALTER TABLE User2
-RENAME TO Customer1;
